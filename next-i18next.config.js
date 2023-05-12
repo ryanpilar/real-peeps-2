7d975eea6e62178a7582b2989f95f16c10c883f2
ryanpilar <ryan_pilar@outlook.com>
2023-05-11 20:38:11 -0600

    changed config values to be empty and have nodefaults  next-i18Next.config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,10 @@ module.exports = {
   i18n: {
     // locales: ["en", "de", "es", "ar", "he", "zh"],
     // defaultLocale: "en",
+    // localeDetection: false,
+    locales: [],
+    defaultLocale: null,
     localeDetection: false,
   },
-  //   localePath: path.resolve("./public/locales"),
+  localePath: path.resolve("./public/locales"),
 };
