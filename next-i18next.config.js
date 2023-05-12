11fb9d310b0a5df3749ac4e9e9e0f1d0d6390544
ryanpilar <ryan_pilar@outlook.com>
2023-05-11 21:13:57 -0600

    commented out all i18 in config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 module.exports = {
-  i18n: {
-    // locales: ["en", "de", "es", "ar", "he", "zh"],
-    // defaultLocale: "en",
-    // localeDetection: false,
-    locales: none,
-    defaultLocale: none,
-    localeDetection: false,
-  },
-  localePath: path.resolve("./public/locales"),
+  //   i18n: {
+  //     // locales: ["en", "de", "es", "ar", "he", "zh"],
+  //     // defaultLocale: "en",
+  //     // localeDetection: false,
+  //     locales: none,
+  //     defaultLocale: none,
+  //     localeDetection: false,
+  //   },
+  //   localePath: path.resolve("./public/locales"),
 };
